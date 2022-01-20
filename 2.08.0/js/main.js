0fc0db16c1d50da91deadd0292860b79aec4cb49
Joshua Trimm <trimmj@etsu.edu>
2022-01-20 16:05:07 -0500

    updated readme

--- 2.08.0/js/main.js
+++ 2.08.0/js/main.js
@@ -3,6 +3,9 @@
  *    Mastering Data Visualization with D3.js
  *    2.8 - Activity: Your first visualization!
  */
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
 
 d3.json("data/buildings.json")
   .then((data) => {
@@ -10,22 +13,37 @@ d3.json("data/buildings.json")
       d.height = Number(d.height);
     });
 
-    const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.height)]) // typically will scale vertically
+      .range([0, HEIGHT]); // typeically will scale horizontally
+    // const x = d3.scalelog().domain([0, 828]).range([0, 400]).base();
+    const x = d3
+      .scaleBand()
+      .domain(data.map((d) => d.name))
+      .range([0, WIDTH])
+      .paddingInner(0.3)
+      .paddingOuter(0.2);
+    console.log(x("Burj Khalifa"));
 
     const svg = d3
       .select("#chart-area")
       .append("svg")
-      .attr("width", 400)
-      .attr("height", 400);
+      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
 
-    const rects = svg.selectAll("rect").data(data);
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+    const rects = g.selectAll("rect").data(data);
 
     rects
       .enter()
       .append("rect")
       .attr("y", 0)
-      .attr("x", (d, i) => i * 60)
-      .attr("width", 40)
+      .attr("x", (d) => x(d.name))
+      .attr("width", x.bandwidth)
       .attr("height", (d) => d.height)
       .attr("fill", "grey");
   })
--- README.md
+++ README.md
@@ -20,6 +20,31 @@
 
 ## Scaling a framed area
 * Domain and range
+* Defining Domain
+  * d3.min, max, extend
+* data.map
+
+
+## Time Scales
+* d3.scaleTime().domain([new Date(2000, 0, 1), new Date(2001, 0, 1)]).range([0, 400])
+
+## Log Scales
+* d3.scaleLog().domain([300, 15000]).range([0, 400]).base(10)
 ![image](https://user-images.githubusercontent.com/16611773/150375719-47afe317-8553-4387-9601-6490cd386188.png)
 
+## Ordinal Scales
+* d3 has color schemes: 
+  * d3.schemeCategory10<>
+  * d3.schemeCategory20<>
+  * d3.schemeCategory20b<>
+  * d3.schemeCategory20c<>
+* d3.scaleOrdinal().domain([ARRAY OF DATA POINTS]).range(d3.schemeCategory10)
 
+## Band Scales
+* Used to space out categories based on how many elements it has. 
+* d3.scaleBand().domain([ARRAY OF DATA POINTS]).range([0, 400]).paddingInner(0.3). paddingOuter(0.2)
+
+
+## SVG Groups
+* used to bundle graphical elements together
+* transformation attr can move the whole group
